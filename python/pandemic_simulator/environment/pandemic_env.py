8a1b2d420c2bf20ff5d8de721028da2adf353ab1
alfred100p <alfredwilliamjacob@gmail.com>
2022-04-27 10:54:13 -0500

    a

---
+++ python/pandemic_simulator/environment/pandemic_env.py
@@ -0,0 +1,313 @@
+# Confidential, Copyright 2020, Sony Corporation of America, All rights reserved.
+from collections import UserString
+from typing import List, Optional, Dict, Tuple, Mapping, Type, Sequence
+
+import gym
+
+from .done import DoneFunction
+from .interfaces import LocationID, PandemicObservation, NonEssentialBusinessLocationState, PandemicRegulation, \
+    InfectionSummary
+from .pandemic_sim import PandemicSim
+from .reward import RewardFunction, SumReward, RewardFunctionFactory, RewardFunctionType
+from .simulator_config import PandemicSimConfig
+from .simulator_opts import PandemicSimOpts
+
+__all__ = ['PandemicGymEnv']
+
+
+class PandemicGymEnv(gym.Env):
+    """A gym environment interface wrapper for the Pandemic Simulator."""
+
+    _pandemic_sim: PandemicSim
+    _stage_to_regulation: Mapping[int, PandemicRegulation]
+    _obs_history_size: int
+    _sim_steps_per_regulation: int
+    _non_essential_fbusiness_loc_ids: Optional[List[LocationID]]
+    _reward_fn: Optional[RewardFunction]
+    _done_fn: Optional[DoneFunction]
+
+    _last_observation: PandemicObservation
+    _last_reward: float
+
+    def __init__(self,
+                 pandemic_sim: PandemicSim,
+                 pandemic_regulations: Sequence[PandemicRegulation],
+                 reward_fn: Optional[RewardFunction] = None,
+                 done_fn: Optional[DoneFunction] = None,
+                 obs_history_size: int = 1,
+                 sim_steps_per_regulation: int = 1,
+                 non_essential_business_location_ids: Optional[List[LocationID]] = None,
+                 ):
+        """
+        :param pandemic_sim: Pandemic simulator instance
+        :param pandemic_regulations: A sequence of pandemic regulations
+        :param reward_fn: reward function
+        :param done_fn: done function
+        :param obs_history_size: number of latest sim step states to include in the observation
+        :param sim_steps_per_regulation: number of sim_steps to run for each regulation
+        :param non_essential_business_location_ids: an ordered list of non-essential business location ids
+        """
+        self.env=self
+        self._max_episode_steps=24
+        self._pandemic_sim = pandemic_sim
+        self._stage_to_regulation = {reg.stage: reg for reg in pandemic_regulations}
+        self._obs_history_size = obs_history_size
+        self._sim_steps_per_regulation = sim_steps_per_regulation
+
+        if non_essential_business_location_ids is not None:
+            for loc_id in non_essential_business_location_ids:
+                assert isinstance(self._pandemic_sim.state.id_to_location_state[loc_id],
+                                  NonEssentialBusinessLocationState)
+        self._non_essential_business_loc_ids = non_essential_business_location_ids
+
+        self._reward_fn = reward_fn
+        self._done_fn = done_fn
+
+        self.action_space = gym.spaces.Discrete(len(self._stage_to_regulation))
+
+    @classmethod
+    def from_config(cls: Type['PandemicGymEnv'],
+                    sim_config: PandemicSimConfig,
+                    pandemic_regulations: Sequence[PandemicRegulation],
+                    sim_opts: PandemicSimOpts = PandemicSimOpts(),
+                    reward_fn: Optional[RewardFunction] = None,
+                    done_fn: Optional[DoneFunction] = None,
+                    name: Optional[UserString]=None,
+                    obs_history_size: int = 1,
+                    non_essential_business_location_ids: Optional[List[LocationID]] = None,
+                    ) -> 'PandemicGymEnv':
+        """
+        Creates an instance using config
+
+        :param sim_config: Simulator config
+        :param pandemic_regulations: A sequence of pandemic regulations
+        :param sim_opts: Simulator opts
+        :param reward_fn: reward function
+        :param done_fn: done function
+        :param obs_history_size: number of latest sim step states to include in the observation
+        :param non_essential_business_location_ids: an ordered list of non-essential business location ids
+        """
+        sim = PandemicSim.from_config(sim_config, sim_opts, name)
+
+        if sim_config.max_hospital_capacity == -1:
+            raise Exception("Nothing much to optimise if max hospital capacity is -1.")
+
+        reward_fn = reward_fn or SumReward(
+            reward_fns=[
+                RewardFunctionFactory.default(RewardFunctionType.INFECTION_SUMMARY_ABOVE_THRESHOLD,
+                                              summary_type=InfectionSummary.CRITICAL,
+                                              threshold=sim_config.max_hospital_capacity),
+                RewardFunctionFactory.default(RewardFunctionType.LOWER_STAGE,
+                                              num_stages=len(pandemic_regulations)),
+                RewardFunctionFactory.default(RewardFunctionType.SMOOTH_STAGE_CHANGES,
+                                              num_stages=len(pandemic_regulations))
+            ],
+            weights=[.4,.1, 0.02]
+        )
+
+        return PandemicGymEnv(pandemic_sim=sim,
+                              pandemic_regulations=pandemic_regulations,
+                              sim_steps_per_regulation=sim_opts.sim_steps_per_regulation,
+                              reward_fn=reward_fn,
+                              done_fn=done_fn,
+                              obs_history_size=obs_history_size,
+                              non_essential_business_location_ids=non_essential_business_location_ids)
+
+    @property
+    def pandemic_sim(self) -> PandemicSim:
+        return self._pandemic_sim
+
+    @property
+    def observation(self) -> PandemicObservation:
+        return self._last_observation
+
+    @property
+    def last_reward(self) -> float:
+        return self._last_reward
+
+    def step(self, action: int) -> Tuple[PandemicObservation, float, bool, Dict]:
+        assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
+
+        # execute the action if different from the current stage
+        if action != self._last_observation.stage[-1, 0, 0]:  # stage has a TNC layout
+            regulation = self._stage_to_regulation[action]
+            self._pandemic_sim.impose_regulation(regulation=regulation)
+
+        # update the sim until next regulation interval trigger and construct obs from state hist
+        obs = PandemicObservation.create_empty(
+            history_size=self._obs_history_size,
+            num_non_essential_business=len(self._non_essential_business_loc_ids)
+            if self._non_essential_business_loc_ids is not None else None)
+
+        hist_index = 0
+        for i in range(self._sim_steps_per_regulation):
+            # step sim
+            self._pandemic_sim.step()
+
+            # store only the last self._history_size state values
+            if i >= (self._sim_steps_per_regulation - self._obs_history_size):
+                obs.update_obs_with_sim_state(self._pandemic_sim.state, hist_index,
+                                              self._non_essential_business_loc_ids)
+                hist_index += 1
+
+        prev_obs = self._last_observation
+        self._last_reward = self._reward_fn.calculate_reward(prev_obs, action, obs) if self._reward_fn else 0.
+        done = self._done_fn.calculate_done(obs, action) if self._done_fn else False
+        self._last_observation = obs
+
+        #print(self.env._last_observation)
+        return self._last_observation, self._last_reward, done, {}
+
+    def reset(self) -> PandemicObservation:
+        self._pandemic_sim.reset()
+        self._last_observation = PandemicObservation.create_empty(
+            history_size=self._obs_history_size,
+            num_non_essential_business=len(self._non_essential_business_loc_ids)
+            if self._non_essential_business_loc_ids is not None else None)
+        self._last_reward = 0.0
+        if self._done_fn is not None:
+            self._done_fn.reset()
+        return self._last_observation.obs
+
+    def render(self, mode: str = 'human') -> bool:
+        pass
+
+class PandemicGymEnvWrapper(gym.ActionWrapper):
+
+    _pandemic_sim: PandemicSim
+    _stage_to_regulation: Mapping[int, PandemicRegulation]
+    _obs_history_size: int
+    _sim_steps_per_regulation: int
+    _non_essential_business_loc_ids: Optional[List[LocationID]]
+    _reward_fn: Optional[RewardFunction]
+    _done_fn: Optional[DoneFunction]
+
+    _last_observation: PandemicObservation
+    _last_reward: float
+    
+    def __init__(self,env,warmup):
+        super(PandemicGymEnvWrapper,self).__init__(env)
+        self.env=env
+        self.action_space=gym.spaces.Discrete(3,)
+        self.warmup=warmup
+
+    def reset(self) -> PandemicObservation:
+        self.env.reset()
+        self.warmup=True
+        return self.env._last_observation.obs
+
+
+    def action(self, act):
+        '''
+        act: action value from [-1,0,1]
+            -1 Reduce Stage by 1
+            0 Maintain Current Stage
+            1 Increase Stage by 1 
+        '''
+        if self.warmup:
+            #print('warmup')
+            if self.env._last_observation.obs[11]:
+                if self.env._last_observation.stage[0,0,0]<4:
+                    return 1+int(self.env._last_observation.stage[0,0,0])
+                else:
+                    self.warmup=False
+                    #print('warmup over'+str(self.env._last_observation.obs[12]*120))
+                    
+                    return int(self.env._last_observation.stage[0,0,0])
+            else:
+                if self.env._last_observation.stage[0,0,0]>0:
+                    return int(self.env._last_observation.stage[0,0,0])-1
+                else:
+                    return int(self.env._last_observation.stage[0,0,0])
+                
+        else:
+            return int(max(0,min(act-1+self.env._last_observation.stage[-1, 0, 0],len(self.env._stage_to_regulation)-1)))
+
+    def raction(self, act):
+        '''
+        act: action value from [-1,0,1]
+            -1 Reduce Stage by 1
+            0 Maintain Current Stage
+            1 Increase Stage by 1 
+        '''
+
+        return int(max(0,min(act-1+self.env._last_observation.stage[-1, 0, 0],len(self.env._stage_to_regulation)-1)))
+
+
+
+    def step(self, act: int) -> Tuple[PandemicObservation, float, bool, Dict]:
+        assert self.action_space.contains(act), "%r (%s) invalid" % (act, type(act))
+        act=self.action(act)
+        
+
+        # execute the action if different from the current stage
+        if act != self.env._last_observation.stage[-1, 0, 0]:  # stage has a TNC layout
+            regulation = self.env._stage_to_regulation[act]
+            self.env._pandemic_sim.impose_regulation(regulation=regulation)
+
+        # update the sim until next regulation interval trigger and construct obs from state hist
+        obs = PandemicObservation.create_empty(
+            history_size=self.env._obs_history_size,
+            num_non_essential_business=len(self.env._non_essential_business_loc_ids)
+            if self.env._non_essential_business_loc_ids is not None else None)
+
+        hist_index = 0
+        for i in range(self.env._sim_steps_per_regulation):
+            # step sim
+            self.env._pandemic_sim.step()
+
+            # store only the last self._history_size state values
+            if i >= (self.env._sim_steps_per_regulation - self.env._obs_history_size):
+                obs.update_obs_with_sim_state(self.env._pandemic_sim.state, hist_index,
+                                              self.env._non_essential_business_loc_ids)
+                hist_index += 1
+
+        prev_obs = self.env._last_observation
+        self.env._last_reward = self.env._reward_fn.calculate_reward(prev_obs, act, obs) if self.env._reward_fn else 0.
+        done = self.env._done_fn.calculate_done(obs, act) if self.env._done_fn else False
+        self.env._last_observation = obs
+        #print(self.env._last_observation)
+        #self.env._pandemic_sim.viz.record(self.env._last_observation,self.env._last_reward)
+        #print(self.env._last_reward)
+        return self.env._last_observation.obs, self.env._last_reward, done, {}
+    
+    def rstep(self, act: int) -> Tuple[PandemicObservation, float, bool, Dict]:
+        assert self.action_space.contains(act), "%r (%s) invalid" % (act, type(act))
+        act=self.raction(act)
+        
+
+        # execute the action if different from the current stage
+        if act != self.env._last_observation.stage[-1, 0, 0]:  # stage has a TNC layout
+            regulation = self.env._stage_to_regulation[act]
+            self.env._pandemic_sim.impose_regulation(regulation=regulation)
+
+        # update the sim until next regulation interval trigger and construct obs from state hist
+        obs = PandemicObservation.create_empty(
+            history_size=self.env._obs_history_size,
+            num_non_essential_business=len(self.env._non_essential_business_loc_ids)
+            if self.env._non_essential_business_loc_ids is not None else None)
+
+        hist_index = 0
+        for i in range(self.env._sim_steps_per_regulation):
+            # step sim
+            self.env._pandemic_sim.step()
+
+            # store only the last self._history_size state values
+            if i >= (self.env._sim_steps_per_regulation - self.env._obs_history_size):
+                obs.update_obs_with_sim_state(self.env._pandemic_sim.state, hist_index,
+                                              self.env._non_essential_business_loc_ids)
+                hist_index += 1
+
+        prev_obs = self.env._last_observation
+        self.env._last_reward = self.env._reward_fn.calculate_reward(prev_obs, act, obs) if self.env._reward_fn else 0.
+        done = self.env._done_fn.calculate_done(obs, act) if self.env._done_fn else False
+        self.env._last_observation = obs
+        #print(self.env._last_observation)
+        #self.env._pandemic_sim.viz.record(self.env._last_observation,self.env._last_reward)
+        #print(self.env._last_reward)
+
+        return self.env._last_observation.obs, self.env._last_reward, done, {}
+    
+    def plot(self):
+        return 0
+        #self.env._pandemic_sim.viz.plot()
---
+++ test.py
@@ -0,0 +1,119 @@
+import os
+import sys
+from os.path import dirname, abspath
+sys.path.append(dirname(dirname(abspath(__file__))))
+
+import gym
+
+from agents.actor_critic_agents.A2C import A2C
+from agents.DQN_agents.Dueling_DDQN import Dueling_DDQN
+from agents.actor_critic_agents.SAC_Discrete import SAC_Discrete
+from agents.actor_critic_agents.A3C import A3C
+from agents.policy_gradient_agents.PPO import PPO
+from agents.Trainer import Trainer
+from utilities.data_structures.Config import Config
+from agents.DQN_agents.DDQN import DDQN
+from agents.DQN_agents.DDQN_With_Prioritised_Experience_Replay import DDQN_With_Prioritised_Experience_Replay
+from agents.DQN_agents.DQN import DQN
+from agents.DQN_agents.DQN_With_Fixed_Q_Targets import DQN_With_Fixed_Q_Targets
+from python.pandemic_simulator.environment.pandemic_env import PandemicGymEnvWrapper
+from tqdm import trange
+
+import pandemic_simulator as ps
+
+ps.init_globals(seed=0)
+sim_config = ps.sh.small_town_config
+
+viz = ps.viz.GymViz.from_config(sim_config=sim_config)
+
+env = ps.env.PandemicGymEnv.from_config(name='test', sim_config=sim_config, pandemic_regulations=ps.sh.austin_regulations,done_fn=ps.env.done.ORDone(done_fns=[ps.env.done.InfectionSummaryAboveThresholdDone(summary_type=ps.env.infection_model.InfectionSummary.CRITICAL,threshold=sim_config.max_hospital_capacity*3),ps.env.done.NoPandemicDone(num_days=30),ps.env.done.NoMoreInfectionsDone()]))
+
+
+p_env=  PandemicGymEnvWrapper(env=env,warmup=True)
+
+config = Config()
+config.seed = 1
+config.environment = p_env
+config.num_episodes_to_run = 8000
+config.file_to_save_data_results = "results/data_and_graphs/PandemicSim_Results_Data.pkl"
+config.file_to_save_results_graph = "results/data_and_graphs/PandemicSim_Results_Graph.png"
+config.show_solution_score = False
+config.visualise_individual_results = False
+config.visualise_overall_agent_results = True
+config.standard_deviation_results = 1.0
+config.runs_per_agent = 1
+config.use_GPU = False
+config.overwrite_existing_results_file = False
+config.randomise_random_seed = True
+config.save_model = True
+
+
+config.hyperparameters = {
+    
+    
+    
+
+
+    
+    "Actor_Critic_Agents":  {
+
+        "learning_rate": 0.005,
+        "linear_hidden_units": [20, 10],
+        "final_layer_activation": ["SOFTMAX", None],
+        "gradient_clipping_norm": 5.0,
+        "discount_rate": 0.99,
+        "epsilon_decay_rate_denominator": 1.0,
+        "normalise_rewards": True,
+        "exploration_worker_difference": 2.0,
+        "clip_rewards": False,
+
+        "Actor": {
+
+            "learning_rate": 0.01,
+
+            "linear_hidden_units": [128],
+            "final_layer_activation": "Softmax",
+            "batch_norm": False,
+            "tau": 0.005,
+            "gradient_clipping_norm": 5,
+            "initialiser": "Xavier"
+        },
+
+        "Critic": {
+            "learning_rate": 0.03,
+
+            "linear_hidden_units": [128],
+            "final_layer_activation": None,
+            "batch_norm": False,
+            "buffer_size": 1000,
+            "tau": 0.005,
+            "gradient_clipping_norm": 5,
+            "initialiser": "Xavier"
+        },
+
+        "min_steps_before_learning": 50,
+        "batch_size": 16,
+        "discount_rate": 0.99,
+        "mu": 0.0, #for O-H noise
+        "theta": 0.15, #for O-H noise
+        "sigma": 0.25, #for O-H noise
+        "action_noise_std": 0.2,  # for TD3
+        "action_noise_clipping_range": 0.5,  # for TD3
+        "update_every_n_steps": 30,
+        "learning_updates_per_learning_session": 8000,
+        "automatically_tune_entropy_hyperparameter": True,
+        "entropy_term_weight": None,
+        "add_extra_noise": False,
+        "do_evaluation_iterations": True
+    }
+}
+    
+
+if __name__ == "__main__":
+
+    #os.mkdir(os.path.dirname(os.getcwd())+'/plots')
+
+    AGENTS = [SAC_Discrete, ]#DDQN, Dueling_DDQN, DQN, DQN_With_Fixed_Q_Targets,
+              #DDQN_With_Prioritised_Experience_Replay, A2C, PPO, A3C ]
+    trainer = Trainer(config, AGENTS)
+    trainer.run_games_for_agents(load=False)
