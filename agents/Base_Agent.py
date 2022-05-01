5489272971d5222f30da6991b74a8504091fae48
Alfred W. Jacob <82844187+alfred100p@users.noreply.github.com>
2022-04-30 21:09:51 -0500

    Update Base_Agent.py

--- agents/Base_Agent.py
+++ agents/Base_Agent.py
@@ -354,7 +354,7 @@ class Base_Agent(object):
         
         if load: 
             
-            net2.load_state_dict(torch.load("Models/123{}_network.pt".format(self.agent_name+name)))
+            net2.load_state_dict(torch.load("/u/alfredwj/home/l5/123{}_network.pt".format(self.agent_name+name)))
         return net2
 
     def turn_on_any_epsilon_greedy_exploration(self):
